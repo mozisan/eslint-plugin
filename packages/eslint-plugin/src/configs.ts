98238294d8dc8f0a17c6877e4ffe304c7a8416d7
('3') <mozisan@users.noreply.github.com>
2023-08-03 22:16:28 +0900

    feat(eslint-plugin): add esm-classic config

--- packages/eslint-plugin/src/configs.ts
+++ packages/eslint-plugin/src/configs.ts
@@ -3,11 +3,13 @@ import type { TSESLint } from "@typescript-eslint/utils";
 import cjs from "./configs/cjs";
 import cjsModern from "./configs/cjs-modern";
 import esm from "./configs/esm";
+import esmClassic from "./configs/esm-classic";
 import overrides from "./overrides";
 
 export = {
 	cjs,
 	"cjs-modern": cjsModern,
 	esm,
+	"esm-classic": esmClassic,
 	...overrides,
 } satisfies Record<string, TSESLint.Linter.Config>;
---
+++ packages/eslint-plugin/src/configs/esm-classic.ts
@@ -0,0 +1,33 @@
+import type { TSESLint } from "@typescript-eslint/utils";
+
+export = {
+	extends: [require.resolve("./bases/base"), require.resolve("./bases/classic")],
+	overrides: [
+		{
+			files: ["*.cjs"],
+			parserOptions: {
+				ecmaVersion: "latest",
+				sourceType: "script",
+			},
+			env: {
+				node: true,
+			},
+			rules: {
+				// eslint-plugin-import
+				"import/no-import-module-exports": "error",
+				// eslint-plugin-unicorn
+				"unicorn/prefer-module": "off",
+			},
+		},
+		{
+			files: ["*.?(m)js"],
+			parserOptions: {
+				ecmaVersion: "latest",
+				sourceType: "module",
+			},
+			env: {
+				node: true,
+			},
+		},
+	],
+} satisfies TSESLint.Linter.Config;
